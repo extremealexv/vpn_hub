506416ff76720814ad3bb6cbdd06f2eb405d1aa8
extremealexv <alexander.vasilyev83@gmail.com>
2026-07-02 06:12:17 -0400

    Make windows build script dynamically locate customtkinter for pyinstaller

--- windows_client/build.bat
+++ windows_client/build.bat
@@ -1,6 +1,11 @@
 @echo off
 echo Building VPN Hub Windows Client...
 pip install -r requirements.txt
-pyinstaller --noconfirm --onedir --windowed --name "VPNHubClient" --add-data "%localappdata%\Programs\Python\Python310\Lib\site-packages\customtkinter;customtkinter/" "app.py"
+
+FOR /F "tokens=*" %%g IN ('python -c "import customtkinter, os; print(os.path.dirname(customtkinter.__file__))"') do (SET CTK_PATH=%%g)
+
+echo CustomTkinter located at: %CTK_PATH%
+pyinstaller --noconfirm --onedir --windowed --name "VPNHubClient" --add-data "%CTK_PATH%;customtkinter/" "app.py"
+
 echo Build complete! You can find the executable in the dist/VPNHubClient/ folder.
 pause
